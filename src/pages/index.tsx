ec5f57bafbfbbd0c38651c7d97f3226f2f2b5f58
Ramon Moreira <ramon.dmm@gmail.com>
2020-12-16 18:11:15 -0300

    Adding Axios in index page and make a first get from data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,6 @@
+import axios from 'axios'
+import { ResultProps } from 'types/api'
+
 import Main from 'components/Main'
 
 const Index = () => (
@@ -5,4 +8,14 @@ const Index = () => (
     <Main />
   </>
 )
+
+Index.getInitialProps = async () => {
+  const res = await axios.get(
+    'https://voliveira.s3-sa-east-1.amazonaws.com/sneakers/index.json'
+  )
+  const products: ResultProps[] | undefined = await res.data
+  console.log(products)
+  return { products: products }
+}
+
 export default Index
