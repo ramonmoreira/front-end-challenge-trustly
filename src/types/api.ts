51c49cf5ad0d6a31414c45119d1c0ca1be360762
Ramon Moreira <ramon.dmm@gmail.com>
2020-12-16 18:07:26 -0300

    Creating types to recive api

---
+++ src/types/api.ts
@@ -0,0 +1,16 @@
+export type ResultProps = {
+    id?: string
+    description: string
+    color?: string
+    price: string
+    currency?: string
+    thumbnailURL: string
+    maxresURL?: string
+}
+export type ProductsProps = {
+    results: ResultProps[]
+}
+
+export type ProductsPageProps = {
+    products: ProductsProps
+}
