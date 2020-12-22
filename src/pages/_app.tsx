a9a0c94cd81cdac55b11d894280a91544a43cbc9
Ramon Moreira <ramon.dmm@gmail.com>
2020-12-22 01:19:39 -0300

    Making HeaderNav workable

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import { AppProps } from 'next/app'
 import Head from 'next/head'
 
 import GlobalStyles from 'styles/global.styles'
+import HeaderNav from 'components/HeaderNav'
 
 function App({ Component, pageProps }: AppProps) {
   return (
@@ -20,6 +21,7 @@ function App({ Component, pageProps }: AppProps) {
         />
       </Head>
       <GlobalStyles />
+      <HeaderNav />
       <Component {...pageProps} />
     </>
   )
